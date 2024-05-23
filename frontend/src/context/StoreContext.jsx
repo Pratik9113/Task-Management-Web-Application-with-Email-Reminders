1357dd9619d5283f8302927790d5766dc67f961e
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-05-23 11:53:17 +0530

    add env

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
-    const url = "http://localhost:4000"
+    const url = process.env.VITE_BASE_URL;
     const [token, setToken] = useState("");
     const contextValue = {
         token,
