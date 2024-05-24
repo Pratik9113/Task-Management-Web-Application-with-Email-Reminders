e5a5f651fc893ed69fb7d5aa827ec5db2d73c9bc
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-05-24 16:26:12 +0530

    add gitignore file

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 export const connectDB = async () =>{
     try {
-        await mongoose.connect('mongodb+srv://pratikpatil9113:up6EXxSVmMTZPpFp@cluster0.nuf9zfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect(`${process.env.mongo_url}`);
         console.log("db connected");
     } catch (error) {
         console.error("error ", error);
